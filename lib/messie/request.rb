69b4cd15cc93a3e65a7e5fb1b90c528fed48a0f3
Dominik Liebler <liebler.dominik@googlemail.com>
2012-03-26 22:30:19 +0200

    docs

--- lib/messie/request.rb
+++ lib/messie/request.rb
@@ -3,21 +3,27 @@ $:.unshift(File.dirname(__FILE__))
 # internal
 require 'user_agent'
 require 'response'
+
+# external
 require 'openssl'
 require 'uri'
 
 module Messie
 
-  # encapsulates the HTTP request
+  # Public: encapsulates the HTTP request
+  #
+  # Examples:
+  #   request = Messie::Request.new("http://localhost")
+  #   request.add_header('foo', 'bar')
+  #   request.user_agent('foobar_user_agent_1.0')
+  #   response = request.crawl
   class Request
 
     attr_reader :headers, :uri
 
-    # init request and set standard parameters
+    # Public: inits the request and sets standard parameters
     #
     # uri - a String or URI object to be crawled
-    #
-    # Returns: a new Messie::Request object
     def initialize uri
       @headers = {
         'User-Agent' => Messie::UserAgent.new.to_s,
@@ -41,49 +47,71 @@ module Messie
       @uri = uri
     end
 
-    # get the response of the crawling
+    # Public: crawl the page and get the Messie::Response object
     #
-    def crawl
-      if @response.nil?
+    # force_reload - True or False that allows for requesting a
+    #                fresh instance of the Messie::Response object
+    #
+    # Returns: a Messie::Response object
+    def crawl(force_reload = false)
+      if @response.nil? or force_reload
         @response = crawl_and_follow
       end
 
       @response
     end
 
-    # set a HTTP request header
+    # Public: set a HTTP request header
     #
+    # key - a valid HTTP String header name
+    # value - String value for the key
+    #
+    # Returns: a String describing the set value
     def add_header(key, value)
       @headers[key] = value
     end
 
-    # method missing to respond to setting of headers
+    # Internal: method missing to respond to setting of headers
     # with dynamic methods
     #
-    def method_missing(m, *args, &block)
-      key = m.to_s.split('_').map {|x| x.capitalize }.join('-')
+    # method - a String method name
+    # args - an Array of arguments supplied
+    # block - a Block supplied to the called method
+    #
+    # Returns: a String describing the set value
+    def method_missing(method, *args, &block)
+      key = method.to_s.split('_').map {|x| x.capitalize }.join('-')
       value = args.shift
 
       add_header(key, value)
     end
 
-    # sets the cert store to use if requesting HTTPS resources
-    # @param store OpenSSL::X509::Store
-    def ssl_cert_store store
+    # Public: sets the cert store to use if requesting HTTPS resources
+    #
+    # store - an OpenSSL::X509::Store object
+    #
+    # Returns: the set OpenSSL::X509::Store object
+    def ssl_cert_store(store)
       @ssl_cert_store = store
     end
 
-    # set the SSL verify mode, either OpenSSL::SSL::VERIFY_PEER or OpenSSL::SSL::VERIFY_NONE
-    # @param mode
-    def ssl_verify_mode mode
+    # Public: sets the SSL verify mode, either OpenSSL::SSL::VERIFY_PEER or OpenSSL::SSL::VERIFY_NONE
+    #
+    # mode - the set Fixnum SSL mode
+    #
+    # Returns: Fixnum the set SSL mode
+    def ssl_verify_mode(mode)
       @ssl_verify_mode = mode
     end
 
     private
 
-    # crawl the page and follow HTTP redirects
+    # Internal: crawls the page and follows HTTP redirects (if any)
     #
-    def crawl_and_follow limit = 5
+    # limit - a Fixnum counting the max HTTP redirects
+    #
+    # Returns: a Messie::Response object
+    def crawl_and_follow(limit = 5)
       fail 'http redirect too deep' if limit.zero?
 
       start = Time.new
@@ -120,7 +148,9 @@ module Messie
       end
     end
 
-    # get the request path
+    # Internal: returns the request path (e.g. /foo/bar for http://localhost/foo/bar)
+    #
+    # Returns: a String describing the request path
     def request_path
       if @uri.path.length == 0
         '/'
