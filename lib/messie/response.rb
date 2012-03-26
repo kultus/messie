2d0a11137b1d2d0c953abb447f6a8e25447fe7d7
Dominik Liebler <liebler.dominik@googlemail.com>
2012-03-26 22:45:16 +0200

    docs

--- lib/messie/response.rb
+++ lib/messie/response.rb
@@ -1,17 +1,25 @@
 $:.unshift(File.dirname(__FILE__))
 
+# internal
 require 'encoding/plain'
 require 'encoding/deflate'
 require 'encoding/gzip'
 
 module Messie
 
-  # a crawling response
+  # Public: the response of a crawling
   #
   class Response
     attr_reader :code, :body, :time, :headers, :uri
 
-    # factory method to create from net/http response
+    # Public: creates Messie::Response objects from a net/http response
+    #
+    # uri             - a String or URI object
+    # response        - a Net::HTTPResponse response object
+    # response_time   - the time the response took as a Float
+    # request_headers - a Hash containing the headers that were used for the request
+    #
+    # Returns: Messie::Response
     def self.create(uri, response, response_time, request_headers)
       headers = {}
       response.each_header do |key, value|
@@ -19,6 +27,7 @@ module Messie
       end
 
       body = self.decode(headers[:content_encoding], response.body)
+      uri = URI.parse unless uri.kind_of? URI
 
       self.new({
         :uri  => uri,
@@ -30,7 +39,10 @@ module Messie
       })
     end
 
-    def initialize(data={})
+    # Internal: inits a new Messie::Response object
+    #           should not used, better use Messie::Response.create() instead!
+    #
+    def initialize(data = {})
       @uri = data[:uri]
       @code = data[:code]
       @time = data[:time]
@@ -39,7 +51,15 @@ module Messie
       @request_headers = data[:request_headers]
     end
 
-    # convert to a hash
+    # Public: serializes the Messie::Response to a Hash
+    #
+    # Returns: a Hash containing the following keys:
+    #          :uri - the URI of the request
+    #          :code - the HTTP status code as a Fixnum
+    #          :body - the String body of the requested page
+    #          :time - the response time in seconds as a Float
+    #          :response_headers - a Hash of the response headers
+    #          :request_headers - a Hash of the request headers
     def to_h
       {
         :uri  => @uri,
@@ -53,16 +73,26 @@ module Messie
 
     private
 
-    # gzipped content
+    # Internal: decodes the content of the page with the given algorithm
     #
+    # algorithm - a Symbol or String for the used algorithm (Gzip or Deflate)
+    # body - the response body as a String
+    #
+    # Returns: the decoded Messie::Response body as a String
     def self.decode(algorithm, body)
       decoder = self.lookup_decoder(algorithm)
       decoder = decoder.new body
       decoder.decode
     end
 
-    # search for the correct algorithm class
+    # Internal: searches for the correct algorithm class to be used to decode the
+    #           response body
     #
+    # falls back to Messie::Encoding::Plain if the found class doesn't responds to decode()
+    #
+    # algorithm - a Symbol or String
+    #
+    # Returns: a Class used to decode the body
     def self.lookup_decoder(algorithm)
       return Messie::Encoding::Plain if algorithm.to_s.empty?
 
